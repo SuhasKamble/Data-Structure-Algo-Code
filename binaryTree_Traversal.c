9dd79f25df297be71b8291363142fd5f2142a8cf
Suhas <kamblesuhas12@gmail.com>
2021-03-07 11:15:23 +0530

    added binary-tree programs

---
+++ binaryTree_Traversal.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *n;
+    n = (struct Node *)malloc(sizeof(struct Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+int main()
+{
+    struct Node *p = createNode(1);
+    struct Node *p1 = createNode(5);
+    struct Node *p2 = createNode(2);
+    p->left = p1;
+    p->right = p2;
+    preOrder(p);
+    printf("\n");
+    postOrder(p);
+    printf("\n");
+    inOrder(p);
+    return 0;
+}
